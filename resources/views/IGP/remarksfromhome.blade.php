e5c3db481e66bd8c487d9b1b9839156bc6e119d7
hadi <94826933+Iqtidarhadi12@users.noreply.github.com>
2022-01-13 11:57:18 +0500

    final petition reverse

--- resources/views/IGP/remarksfromhome.blade.php
+++ resources/views/IGP/remarksfromhome.blade.php
@@ -809,6 +809,24 @@
                                                                 </div>
                                                                 </div>
                                                             </div>
+                                                            <div class="col-12 px-8 mt-5"
+                                                            >
+                                                            <div class="form-row text-center">
+                                                                <div class="form-group col-md-6">
+                                                                    <a href="{{route('petition-forward',[$petion->id])}}"
+                                                                        class="  mx-2px btn radius-1 border-2 btn-xs btn-brc-tp btn-light-secondary btn-h-lighter-success btn-a-lighter-success bg-success text-white">
+                                                                        Forward <i class="fa fa-forward"></i>
+                                                                    </a>
+                                                                </div>
+                                                                <div class="form-group col-md-6">
+                                                                    <a href="{{route('remarksfromhome')}}"
+                                                                        class="  mx-2px btn radius-1 border-2 btn-xs btn-brc-tp btn-light-secondary btn-h-lighter-success btn-a-lighter-success bg-primary text-white">
+                                                                        Back <i class="fa fa-arrow-left"></i>
+                                                                    </a>
+                                                                </div>
+    
+                                                            </div>
+                                                        </div>
                                                         </div>
 
 
